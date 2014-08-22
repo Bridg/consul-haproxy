ae9c07c96f88b73d471efd63b154fd39ebb6d118
Jared Cugno <jared@bridg.com>
2014-08-22 14:51:40 -0700

    Replacing the [ ] with empty characters from tag Haproxy did not like these

--- watch.go
+++ watch.go
@@ -330,6 +330,8 @@ func formatOutput(inp map[string][]*consulapi.ServiceEntry) map[string][]string 
 			name := fmt.Sprintf("%s_%s_%s", entry.Node.Node, entry.Service.ID, entry.Service.Tags)
 			ip := net.ParseIP(entry.Node.Address)
 			addr := &net.TCPAddr{IP: ip, Port: entry.Service.Port}
+      name := strings.Replace(name, "[", "", -1)
+      name := strings.Replace(name, "]", "", -1)
 			servers[idx] = fmt.Sprintf("server %s %s", name, addr)
 		}
 		out[backend] = servers
